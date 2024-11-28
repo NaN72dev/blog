c177a29817ff4c16e700f1e8f6bcbd88a6afe107
Nan72 <lecongnhan29@gmail.com>
2024-11-28 16:57:23 +0700

    fix(next): remove webpack config & use next config

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,26 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
     // Configure pageExtensions to include md and mdx
-    pageExtensions: ["ts", "tsx", "js", "jsx"],
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
     reactStrictMode: true,
 
     // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
     output: "export",
+    webpack: (config, {defaultLoaders}) => {
+        // https://stackoverflow.com/q/77805373
+        config.module.rules.push({
+            test: "/\.mdx$/",
+            use: [
+                defaultLoaders.babel,
+                {loader: "@mdx-js/loader"}
+            ]
+        });
+        return config;
+    }
 };
 
 // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
+/** @type {import("next").NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withMDX = require("@next/mdx")({
     // Optionally provide remark and rehype plugins
